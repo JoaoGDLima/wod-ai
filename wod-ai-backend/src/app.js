39c8fa8c6784f0e1880a64586599026c940039c1
JoaoGDLima <joaolima@cta.com.br>
2025-05-07 16:26:01 -0300

    [FEATURE]: Cria Front end

--- wod-ai-backend/src/app.js
+++ wod-ai-backend/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import wodRoutes from './routes/wod.routes.js';
 import authRoutes from './routes/auth.routes.js';
@@ -6,6 +7,8 @@ import authRoutes from './routes/auth.routes.js';
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use('/generate-wod', wodRoutes);
---
+++ wod-ai-frontend/src/App.tsx
@@ -0,0 +1,17 @@
+// src/App.jsx
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
